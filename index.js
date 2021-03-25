e48cb170f48b354d25c7854337e0738344c5012c
Rohit Raj <rohit011@protonmail.com>
2021-03-25 22:14:27 +0530

    added local storage

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-var ID = 0;
+var id = 0;
+var todoList = [];
 var tasksAdded = 0;
 var tasksDone = 0;
 var tasksRemaining = 0;
@@ -32,33 +33,91 @@ const counterChange = () => {
   TODORemaining.children[1].innerHTML = tasksRemaining;
 };
 
-counterChange();
-
-const addToDo = (toDo, id) => {
-  const position = "beforeend";
-  const item = `<li id="${id}" class="task-card task-added">
-                    <i class="far fa-circle fa-2x py-1 me-2" onclick="taskDone(this.parentNode, this)"></i>
-                    <span class="flex-grow-1">${toDo}</span>
-                    <i class="fas fa-trash-alt fa-2x py-1 ms-2" onclick="taskTrash(this.parentNode.id)"></i>
-                </li>`;
-
-  list.insertAdjacentHTML(position, item);
+const restoreToDo = () => {
+  let data = localStorage.getItem("TODO");
+  if (data) {
+    todoList = JSON.parse(data);
+    id = todoList.length;
+    tasksAdded = todoList.length;
+    tasksDone = 0;
+    loadToDo(todoList);
+  } else {
+    id = 0;
+    todoList = [];
+    tasksAdded = 0;
+    tasksDone = 0;
+    tasksRemaining = 0;
+  }
 };
 
-const taskDone = (parentNode, node) => {
+const loadToDo = (array) => {
+  array.forEach((element) => {
+    if (element.done) tasksDone++;
+    addToDo(element.name, element.id, element.done, false);
+  });
+};
+
+const addToDo = (toDo, id, done, update) => {
+  const position = "beforeend";
+  var item;
+  if (done) {
+    item = `<li id="${id}" class="task-card task-done">
+              <i class="fas ${CHECK} fa-2x py-1 me-2"></i>
+              <span class="flex-grow-1 ${LINE_THROUGH}">${toDo}</span>
+              <i class="fas fa-trash-alt fa-2x py-1 ms-2" onclick="removeToDo(this.parentNode.id)"></i>
+            </li>`;
+  } else {
+    item = `<li id="${id}" class="task-card task-added">
+              <i class="far ${UNCHECK} fa-2x py-1 me-2" onclick="completeToDo(this.parentNode, this)"></i>
+              <span class="flex-grow-1">${toDo}</span>
+              <i class="fas fa-trash-alt fa-2x py-1 ms-2" onclick="removeToDo(this.parentNode.id)"></i>
+            </li>`;
+  }
+
+  list.insertAdjacentHTML(position, item);
+  if (update) {
+    todoList.push({
+      name: toDo,
+      id: id,
+      done: done,
+    });
+    tasksAdded++;
+  }
+
+  tasksRemaining = tasksAdded - tasksDone;
+  counterChange();
+
+  localStorage.setItem("TODO", JSON.stringify(todoList));
+};
+
+const completeToDo = (parentNode, node) => {
   parentNode.classList.add(TASK_DONE);
   parentNode.classList.remove(TASK_ADDED);
   parentNode.children[1].classList.add(LINE_THROUGH);
   node.classList.add("fas", CHECK);
   node.classList.remove("far", UNCHECK);
 
+  for (let i = 0; i < todoList.length; i++)
+    if (todoList[i].id == parentNode.id) todoList[i].done = true;
+  localStorage.setItem("TODO", JSON.stringify(todoList));
+
   tasksDone++;
   tasksRemaining = tasksAdded - tasksDone;
   counterChange();
 };
 
-const taskTrash = (param) => {
+const removeListToDo = (param) => {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id == param) todoList.splice(i, 1);
+  }
+};
+
+const removeToDo = (param) => {
   document.getElementById(param).remove();
+
+  removeListToDo(param);
+  localStorage.setItem("TODO", JSON.stringify(todoList));
+
   tasksDone > 0 ? tasksDone-- : 0;
   tasksAdded > 0 ? tasksAdded-- : 0;
   tasksRemaining = tasksAdded - tasksDone;
@@ -66,23 +125,16 @@ const taskTrash = (param) => {
 };
 
 clear.addEventListener("click", function () {
-  list.innerHTML = "";
-  ID = 0;
-  tasksAdded = 0;
-  tasksDone = 0;
-  tasksRemaining = 0;
-  counterChange();
+  localStorage.clear();
+  location.reload();
 });
 
 add_to_do.addEventListener("click", function () {
   const toDo = input.value;
   if (toDo) {
-    addToDo(toDo, ID);
+    addToDo(toDo, id, false, true);
     input.value = "";
-    ID++;
-    tasksAdded++;
-    tasksRemaining = tasksAdded - tasksDone;
-    counterChange();
+    id++;
   }
 });
 
@@ -90,12 +142,11 @@ input.addEventListener("keypress", function (event) {
   if (event.key == "Enter") {
     const toDo = input.value;
     if (toDo) {
-      addToDo(toDo, ID);
+      addToDo(toDo, id, false, true);
       input.value = "";
-      ID++;
-      tasksAdded++;
-      tasksRemaining = tasksAdded - tasksDone;
-      counterChange();
+      id++;
     }
   }
 });
+
+window.onload = restoreToDo();
